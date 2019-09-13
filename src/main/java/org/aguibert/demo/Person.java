df872ae19d4789df79427baa239dd2f8adc78343
Andrew Guibert <andy.guibert@gmail.com>
2019-09-12 20:54:09 -0500

    Test with PostgreSQL container

--- src/main/java/org/aguibert/demo/Person.java
+++ src/main/java/org/aguibert/demo/Person.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.eclipse.microprofile.system.test.app;
+package org.aguibert.demo;
 
 import java.util.Objects;
 import java.util.Random;
